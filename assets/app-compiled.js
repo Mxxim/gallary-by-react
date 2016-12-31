38952a447f777530211dbc78e04a6c7db8839ad3
sammy <mamxxim@163.com>
2016-12-31 20:57:05 +0800

    update

--- assets/app-compiled.js
+++ assets/app-compiled.js
@@ -5,7 +5,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 !function (e) {
 	function t(r) {
 		if (n[r]) return n[r].exports;var o = n[r] = { exports: {}, id: r, loaded: !1 };return e[r].call(o.exports, o, o.exports, t), o.loaded = !0, o.exports;
-	}var n = {};return t.m = e, t.c = n, t.p = "/assets/", t(0);
+	}var n = {};return t.m = e, t.c = n, t.p = "assets/", t(0);
 }(function (e) {
 	for (var t in e) {
 		if (Object.prototype.hasOwnProperty.call(e, t)) switch (_typeof(e[t])) {case "function":
